14e672cebc2243fef0e9ca9c1f6df5804ef39336
Anhar Perinkadakkat <anharx@gmail.com>
2024-07-02 17:45:14 +0530

    Blockchain class setup

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,63 @@
+import hashlib
+import json
+import time
+
+from textwrap import dedent
+from uuid import uuid4
+
+from flask import Flask
+
+
 class Blockchain(object):
   def __init__(self):
     self.chain = []
     self.current_transactions = []
+
+    #creates the genesis block
+    self.new_block(previous_hash=1, proof=100)
+
+  
+  def proof_of_work(self,last_proof):
+    """
+      Simple Proof of Work Algorithm:
+        - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+      :param last_proof: <int>
+      :return: <int>
+    """
+
+    proof = 0
+    while self.vaild_proof(last_proof, proof) is False:
+      proof +=1
+
+    return proof
+  
+  @staticmethod
+  def valid_proof(last_proof, proof):
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:4] == "0000"
     
-  def new_block(self):
-    #creates new block and adds it to the chain
-    pass
+  def new_block(self, proof, previous_hash=None):
+    """
+    Create a new Block in the Blockchain
+    :param proof: <int> The proof given by the Proof of Work algorithm
+    :param previous_hash: (Optional) <str> Hash of previous Block
+    :return: <dict> New Block
+    """
+    block = {
+      "index": len(self.chain) + 1,
+      "timestamp": time(),
+      "transactions": self.current_transactions,
+      "proof": proof,
+      "previous_hash": previous_hash or self.hash(self.chain[-1]),
+    }
+
+    #reset current list of transactions
+    self.current_transactions = []
+
+    self.chain.append(block)
+    return block
 
 
   def new_transaction(self, sender, recipient, amount):
@@ -28,8 +80,17 @@ class Blockchain(object):
 
   @staticmethod
   def hash(block):
-    pass
+    """
+    Creates a SHA-256 hash of a Block
+    :param block: <dict> Block
+    :return: <str>
+    """
+
+    #We must make sure that the dictionary is Ordered, or we'll have inconsistent hashes
+    block_string = json.dumps(block, sort_keys=True).encode()
+    return hashlib.sha256(block_string).hexdigest()
 
   @property
   def last_block(self):
-    pass      
+    return self.chain[-1]
+  
